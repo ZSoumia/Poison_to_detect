f540f96bc28b7cf982608c05acc62821aa052573
MKZuziak <maciejzuziak101@gmail.com>
2025-04-23 22:02:35 +0200

    Adds convert_to_hg function to utils

--- ODFL/odfl/dataset_generation/utils.py
+++ ODFL/odfl/dataset_generation/utils.py
@@ -23,54 +23,61 @@ def save_custom_dataset(
     dataset.save_to_disk(path)
 
 
-def save_blueprint(
-    path: str,
-    dataset: dict[datasets.arrow_dataset.Dataset],
-    blueprint_name: str,
-    number_of_clients: int
+def convert_to_hg(
+    dataset: list[dict]
 ):
-    name = os.path.join(path, (blueprint_name + ".csv"))
-    with open(name, "w+") as csv_file:
-        header = ['client_id', 'partition', 'total_samples']
-        labels = dataset[f"node_0_train"].features['label'].names
-        header.extend(labels)
-        csv_file.write(",".join(header) + '\n')
-        
-        translation = False
-        try:
-            labels = [int(label) for label in labels]
-        except ValueError:
-            translation = True
-            labels_translation = {integer: label for (integer, label) in zip(range(len(labels)), labels)}
-            labels = [value for value in labels_translation.keys()]
-        
-        # WRITE ORCHESTRATOR
-        row = ['orchestrator', 'central_test_set', str(len(dataset['orchestrator_data']))]
-        for label in labels:
-            row.append(str(len(dataset['orchestrator_data'].filter(lambda inst: inst['label'] == label))))
-        csv_file.write(",".join(row) + '\n')
-        
-        
-        # WRITE CLIENTS
-        for clients in range(number_of_clients):
-            pass
-        
-        
-        
-        for client, data in enumerate(nodes_data):
-            row = [str(client), 'train_set', str(len(data[0]))]
-            for label in labels:
-                row.append(str(len(data[0].filter(lambda inst: inst['label'] == label))))
-            csv_file.write(",".join(row) + '\n')
+    dataset = [{'image': dataset[index][0], 'label': int(dataset[index][1][0])} for index in range(len(dataset))]
+    dataset = datasets.Dataset.from_list(dataset)
+    return dataset
 
-            row = [str(client), 'test_set', str(len(data[1]))]
-            for label in labels:
-                row.append(str(len(data[1].filter(lambda inst: inst['label'] == label))))
-            csv_file.write(",".join(row) + '\n')
+# def save_blueprint(
+#     path: str,
+#     dataset: dict[datasets.arrow_dataset.Dataset],
+#     blueprint_name: str,
+#     number_of_clients: int
+# ):
+#     name = os.path.join(path, (blueprint_name + ".csv"))
+#     with open(name, "w+") as csv_file:
+#         header = ['client_id', 'partition', 'total_samples']
+#         labels = dataset[f"node_0_train"].features['label'].names
+#         header.extend(labels)
+#         csv_file.write(",".join(header) + '\n')
         
-        #Optional: write labels (if translated)
-        if translation:
-            name = os.path.join(path, (blueprine_name + "translation" + ".txt"))
-            with open(name, "w+") as txt_file:
-                for integer, label in labels_translation.items():
-                    txt_file.write(f"{str(integer)}: {label}")
+#         translation = False
+#         try:
+#             labels = [int(label) for label in labels]
+#         except ValueError:
+#             translation = True
+#             labels_translation = {integer: label for (integer, label) in zip(range(len(labels)), labels)}
+#             labels = [value for value in labels_translation.keys()]
+        
+#         # WRITE ORCHESTRATOR
+#         row = ['orchestrator', 'central_test_set', str(len(dataset['orchestrator_data']))]
+#         for label in labels:
+#             row.append(str(len(dataset['orchestrator_data'].filter(lambda inst: inst['label'] == label))))
+#         csv_file.write(",".join(row) + '\n')
+        
+        
+#         # WRITE CLIENTS
+#         for clients in range(number_of_clients):
+#             pass
+        
+        
+        
+#         for client, data in enumerate(nodes_data):
+#             row = [str(client), 'train_set', str(len(data[0]))]
+#             for label in labels:
+#                 row.append(str(len(data[0].filter(lambda inst: inst['label'] == label))))
+#             csv_file.write(",".join(row) + '\n')
+
+#             row = [str(client), 'test_set', str(len(data[1]))]
+#             for label in labels:
+#                 row.append(str(len(data[1].filter(lambda inst: inst['label'] == label))))
+#             csv_file.write(",".join(row) + '\n')
+        
+#         #Optional: write labels (if translated)
+#         if translation:
+#             name = os.path.join(path, (blueprine_name + "translation" + ".txt"))
+#             with open(name, "w+") as txt_file:
+#                 for integer, label in labels_translation.items():
+#                     txt_file.write(f"{str(integer)}: {label}")
